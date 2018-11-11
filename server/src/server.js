4e8d0d3921cc6bb428221e6be9fd64fa8eaa288b
david-xliu <davxliu@gmail.com>
2018-11-10 23:06:12 -0500

    set up api endpoint for adding todos

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,27 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let { mongoose } = require('../db/mongoose');
 let { Todo } = require('../models/todos');
 let { User } = require('../models/users');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  })
+});
+
+app.listen(3000, () => {
+  console.log('started on port 3000');
+});
 
