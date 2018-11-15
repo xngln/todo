791980d2fca4e8d69786e6013fd6cb43f50bde59
david-xliu <davxliu@gmail.com>
2018-11-15 00:25:10 -0500

    test delete todos by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,41 @@ describe('GET /todos/:id', () => {
       .end(done);
   })
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let id = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(id).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((err) => done());
+      })
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    let fakeId = (new ObjectID()).toHexString;
+    request(app)
+      .delete(`/todos/${fakeId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete(`/todos/${123}`)
+      .expect(404)
+      .end(done);
+  });
+});
