31aa38cbae309bd4a421396715c02fd2995c3230
david-xliu <davxliu@gmail.com>
2018-11-15 00:39:19 -0500

    add PATCH /todos/:id

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { ObjectID } = require('mongodb');
 const { mongoose } = require('../db/mongoose');
@@ -66,6 +67,31 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send(err);
   });
 })
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+  
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({ todo });
+  })
+
+});
 
 app.listen(port, () => {
   console.log(`started up at port ${port}`);
