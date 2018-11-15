faf41583d24b8a4e3eac74da5097c4b942136b8a
david-xliu <davxliu@gmail.com>
2018-11-15 00:08:11 -0500

    add delete todos by id route

--- server/src/server.js
+++ server/src/server.js
@@ -50,6 +50,23 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+})
+
 app.listen(port, () => {
   console.log(`started up at port ${port}`);
 });
