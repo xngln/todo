06c9353c3e0aecda630bc8a79cf9fcfbc7c997b0
david-xliu <davxliu@gmail.com>
2018-11-18 15:02:14 -0500

    hash user password before saving

--- server/playground/hashing.js
+++ server/playground/hashing.js
@@ -1,14 +1,28 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+let password = 'abc123!';
 
-let data = {
-  id: 10
-}
-let token = jwt.sign(data, 'abc123');
-console.log(token);
-let decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+let hashedPass = '$2a$10$qUUqfqeVo8wyb0d5YvaBg.3E4GpNiEPi9g4X3h0Sh3WvVrLprhctm';
+
+bcrypt.compare(password, hashedPass, (err, result) => {
+  console.log(result);
+});
+
+// let data = {
+//   id: 10
+// }
+// let token = jwt.sign(data, 'abc123');
+// console.log(token);
+// let decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
 
 
 // let message = 'I am user number 3';
