11430a9115a9b18c942c60d32ae20dd0c22fa22d
david-xliu <davxliu@gmail.com>
2018-12-02 04:05:57 -0500

    headers file for netlify

--- server/src/server.js
+++ server/src/server.js
@@ -32,7 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   sessionOptions.cookie.secure = true;
   domain = 'https://todoboi.netlify.com';
 }
-
 let allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', domain);
   res.header('Access-Control-Allow-Credentials', 'true');
