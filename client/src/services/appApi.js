7bdec857b9635ed80dc217359f6053af23124e7f
david-xliu <davxliu@gmail.com>
2019-09-29 23:07:47 -0400

    attempt to resurrect this project

---
+++ client/src/services/appApi.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+let baseURL;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000';
+} else {
+  baseURL = 'https://todo-boi.herokuapp.com/';
+}
+
+const Api = () => axios.create({ baseURL });
+
+// Todo endpoints
+const fetchAllTodos = () => Api().get('todos');
+
+const fetchTodoById = id => Api().get(`todos/${id}`);
+
+const postTodos = text => Api().post('todos', { text });
+
+const updateTodos = (id, text, completed) => Api().patch(`todos/${id}`, { text, completed });
+
+const deleteTodo = id => Api().patch(`todos/${id}`);
+
+
+// User endpoints
+const fetchUser = () => Api().get('users/me');
+
+const createUser = (firstName, lastName, email, password) => Api().post('users', { firstName, lastName, email, password });
+
+const loginUser = (email, password) => Api().post('users/login', { email, password }, { withCredentials: true });
+
+const logoutUser = () => Api().delete('users/me/token');
+
+export const todoApi = {
+  fetchAllTodos,
+  fetchTodoById,
+  postTodos,
+  updateTodos,
+  deleteTodo,
+};
+
+export const userApi = {
+  fetchUser,
+  createUser,
+  loginUser,
+  logoutUser,
+};
---
+++ client/src/utils/auth.js
@@ -0,0 +1,17 @@
+// import decode from 'jwt-decode';
+// import axios from 'axios';
+// import auth0 from 'auth0-js';
+// import Router from 'vue-router';
+
+// const ID_TOKEN_KEY = 'id_token';
+// const ACCESS_TOKEN_KEY = 'access_token';
+
+// const CLIENT_ID = '{AUTH0_CLIENT_ID}';
+// const CLIENT_DOMAIN = '{AUTH0_DOMAIN}';
+// const REDIRECT = 'YOUR_CALLBACK_URL';
+// const SCOPE = '{SCOPE}';
+
+// let auth = new auth0.WebAuth({
+//   clientID: CLIENT_ID,
+//   domain: CLIENT_DOMAIN,
+// });
