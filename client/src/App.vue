91297f01c876a2c6207f1aee0dba6cf55ce07b04
david-xliu <davxliu@gmail.com>
2019-10-12 23:52:35 -0400

    fix things

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,8 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-
-  max-width: 1024px;
-  margin: auto;
+  height: 100%;
+  margin: 0;
 }
 </style>
